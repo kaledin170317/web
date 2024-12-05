6630e1684cdf673a780fa3a3227562547c93b5e2
Ney Begrov <317871@edu.itmo.ru>
2024-12-05 13:20:41 +0300

    lab6

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    const highlightActiveMenu = () => {
+        const currentPath = window.location.pathname;
+        document.querySelectorAll('.header__nav a').forEach(link => {
+            link.classList.toggle('active', link.pathname === currentPath);
+        });
+    };
+
+
+    const displayPageLoadTime = () => {
+        const loadTime = Math.round(performance.now());
+        const footer = document.querySelector('footer');
+        if (footer) {
+            footer.insertAdjacentHTML(
+                'beforeend',
+                `<p>Время загрузки страницы: ${loadTime} мс</p>`
+            );
+        }
+    };
+
+
+    window.onload = () => {
+        highlightActiveMenu();
+        displayPageLoadTime();
+    };
+})();
